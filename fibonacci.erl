5f8e93e36fd5594170cb89dfc671a087a6974755
bausano <gtasampserver@seznam.cz>
2017-11-03 06:00:18 +0000

    Fibonacci

--- fibonacci.erl
+++ fibonacci.erl
@@ -6,7 +6,7 @@
 -export([start/0]).
 
 start() ->
-  N = 37,
+  N = 5,
   io:format("Quick: ~p\n\n", [fib2(N)]),
   io:format("Slow: ~p\n\n", [fib(N)]).
 
@@ -21,7 +21,7 @@ fib(0) -> 0;
 %% Our second state would be the second number of the sequence.
 fib(1) -> 1;
 
-%% Recursion ala while loop. As long as N is not 1 or 2, compute previous N.
+%% Recursion ala while loop. As long as N is not 0 or 1, compute previous N.
 %% Also we only want to work with positive numbers, so by specifying
 %% when N > 1 we eliminate infinit loop cases.
 fib(N) when N > 1 ->
@@ -33,11 +33,11 @@ fib(N) when N > 1 ->
 %% is to omit it with underscore.
 fib2(1, A, _) -> A;
 
-%% We want to loop with these 3 arguments as long as N - 1 equals 1.
+%% We want to loop with these 3 arguments and break once N - 1 equals 1.
 fib2(N, A, B) ->
 	fib2(N - 1, A + B, A).
 
 %% Some code sugar. We start the sequence with 1, 0, the first two
 %% numbers of FS. God bless the pattern matching.
-fib2(N) ->
+fib2(N) when N > 0 ->
 	fib2(N, 1, 0).
